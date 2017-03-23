6b563ef51dfb0c10ac9cca9a5ffae57a4c83722a
Rajesh Rana <Rajesh.Rana@rsystems.com>
2017-03-23 19:19:16 +0530

    first Commit again

---
+++ src/main/java/com/example/demo/Library.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+public class Library {
+	
+	public boolean someLibraryMethod(){
+		System.out.println("testing this one");
+		return true;
+	}
+	
+	
+	public boolean unCoveredTest(){
+		System.out.println("testing this uncovered");
+		return true;
+	}
+
+}
---
+++ src/test/java/com/example/demo/LibraryTest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.Test;
+
+public class LibraryTest {
+	
+	@Test
+	public void testSomeLibraryMethod() {
+		Library clasUnderTest = new Library();
+		assert(clasUnderTest.someLibraryMethod());
+	}
+
+}
